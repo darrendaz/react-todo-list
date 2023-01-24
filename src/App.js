78ca2a880dedb4871adc8be740a18dbe69cca16e
Darren Lou <darren.lou@gmail.com>
2023-01-24 10:30:10 -0800

    Update App.js
    * Auto format - double quotes to single quotes

--- src/App.js
+++ src/App.js
@@ -1,94 +1,94 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
 // import Navigation from "./components/navigation"
-import TodoForm from "./components/todoForm"
-import TodoList from "./components/todoList"
+import TodoForm from './components/todoForm';
+import TodoList from './components/todoList';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      greeting: "To Do List",
-      navList: ["Todo List", "Calculator", "Weather Forecast"],
+      greeting: 'To Do List',
+      navList: ['Todo List', 'Calculator', 'Weather Forecast'],
       todoItemIdCounter: 0,
-      todoItem: "",
-      todoList: []
-    }
+      todoItem: '',
+      todoList: [],
+    };
 
-    this.handleTodoItemNameChange = this.handleTodoItemNameChange.bind(this)
-    this.handleCreateTodoItem = this.handleCreateTodoItem.bind(this)
-    this.handleClearList = this.handleClearList.bind(this)
+    this.handleTodoItemNameChange = this.handleTodoItemNameChange.bind(this);
+    this.handleCreateTodoItem = this.handleCreateTodoItem.bind(this);
+    this.handleClearList = this.handleClearList.bind(this);
   }
 
   componentDidMount() {
     this.setState({
-      todoItem: ""
-    })
+      todoItem: '',
+    });
   }
 
   handleTodoItemNameChange(e) {
-    localStorage.setItem("Testing", e.target.value)
+    localStorage.setItem('Testing', e.target.value);
 
     this.setState({
-      todoItem: e.target.value
-    })
+      todoItem: e.target.value,
+    });
   }
 
   handleListItemNameChange(e) {
-    const todoList = this.state.todoList
-    todoList[e.target.id - 1].name = e.target.value
+    const todoList = this.state.todoList;
+    todoList[e.target.id - 1].name = e.target.value;
 
     this.setState({
-      todoList: todoList
-    })
+      todoList: todoList,
+    });
   }
 
   handleCreateTodoItem(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    const todoItemId = this.state.todoItemIdCounter + 1
+    const todoItemId = this.state.todoItemIdCounter + 1;
 
     const todoItemObj = {
       id: todoItemId,
-      name: e.target.todoItem.value || "<_empty_entry_>",
-      completed: false
-    }
+      name: e.target.todoItem.value || '<_empty_entry_>',
+      completed: false,
+    };
 
     this.setState({
-      todoItem: "",
+      todoItem: '',
       todoItemIdCounter: todoItemId,
-      todoList: [...this.state.todoList, todoItemObj]
-    })
+      todoList: [...this.state.todoList, todoItemObj],
+    });
   }
 
   handleDeleteTodoItem(id) {
-    this.setState(prevState => ({
-      todoList: prevState.todoList.filter(todoItem => todoItem.id !== id)
-    }))
+    this.setState((prevState) => ({
+      todoList: prevState.todoList.filter((todoItem) => todoItem.id !== id),
+    }));
   }
 
   toggleComplete(id) {
-    const todoList = this.state.todoList
-    const itemIndex = todoList.findIndex(todoItem => todoItem.id === id)
-    const todoItemObj = todoList[itemIndex]
+    const todoList = this.state.todoList;
+    const itemIndex = todoList.findIndex((todoItem) => todoItem.id === id);
+    const todoItemObj = todoList[itemIndex];
 
-    todoList[itemIndex] = { ...todoItemObj, completed: !todoItemObj.completed }
+    todoList[itemIndex] = { ...todoItemObj, completed: !todoItemObj.completed };
 
-    this.setState({ todoList: todoList })
+    this.setState({ todoList: todoList });
   }
 
   handleClearList(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     this.setState({
       todoItemIdCounter: 0,
-      todoList: []
-    })
+      todoList: [],
+    });
   }
 
   render() {
     return (
-      <div className="container-fluid">
+      <div className='container-fluid'>
         <h1>{this.state.greeting}</h1>
         {/* <Navigation navList={this.state.navList} /> */}
         <TodoForm
@@ -105,8 +105,8 @@ class App extends Component {
           toggleComplete={this.toggleComplete.bind(this)}
         />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
